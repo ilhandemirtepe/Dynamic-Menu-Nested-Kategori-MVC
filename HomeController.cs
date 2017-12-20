c5df01803490449e73bb9cb593524774dd80ad10
ilhan <ilhandemirtepe@gmail.com>
2017-12-20 13:45:49 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DynamicMenu.Models;
+namespace DynamicMenu.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        MenuDBEntities db = new MenuDBEntities();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            ViewBag.menuLevel = db.Menu.Where(m => m.ParentId == null).ToList();
+            return PartialView("Categories");
+        }
+
+
+
+
+        public ActionResult KategoriWithSubCategori()
+        {
+
+
+
+            return PartialView(new ViewModel
+            {
+
+                KategoriListesi = db.Kategori.ToList(),
+                ProductListesi = db.Product.ToList()
+
+
+        });
+        }
+}
+
+
+public class ViewModel
+{
+    public List<Product> ProductListesi { get; set; }
+    public List<Kategori> KategoriListesi { get; set; }
+
+}
+}
